e8e2c0eddb7f481ac3bfffaa03ba22af87dab965
Alex David Frías <adavidfruiz@gmail.com>
2024-05-04 02:01:28 -0500

    Feat: add main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// import './index.css'
+import { ProductApp } from './components/ProductApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ProductApp title={'Lista de Productos!' }/>
+    </React.StrictMode>,
 )
+
