397bc6ebac01259ac28a783a523c8a35395b8353
Alex David Frías <adavidfruiz@gmail.com>
2024-05-04 02:03:54 -0500

    feat: add product service

---
+++ src/services/ProductService.js
@@ -0,0 +1,19 @@
+
+const initProducts = [
+    {
+        id:1,
+        name: 'Monitor Samsung 65',
+        price: 500,
+        description: 'El monitor es increible!'
+    },
+    {
+        id:2,
+        name: 'IPhone 14',
+        price: 800,
+        description: 'El telefono es muy bueno!'
+    }
+];
+
+export const listProduct = () => {
+    return initProducts;
+}
